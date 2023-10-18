ad4d9f7bea3310a282af8f6f809b9a97e77f1049
CristianCazares <cm_cjavier@hotmail.com>
2023-10-18 17:33:35 -0600

    feat(mask.py): added image saving to the refactor

--- scripts/masks.py
+++ scripts/masks.py
@@ -16,18 +16,26 @@ class Mask:
         self,
         video_info,
         amount_of_frames=2,
-        path="../EchoNet-Dynamic/Videos/",
+        videos_path="../EchoNet-Dynamic/Videos/",
         images_files_array=[],
     ):
         self.video_info = video_info
         self.amount_of_frames = amount_of_frames
-        self.path = path
+        self.path = videos_path
         self.images_files_array = images_files_array
 
     def set_images_files_array(self, images_files_array):
         self.images_files_array = images_files_array
 
-    def show(self, mask_type="centroid", show_centroid=False, include_points=False):
+    def generate_masks(
+        self,
+        action="show",
+        mask_type="centroid",
+        show_centroid=False,
+        include_points=False,
+        include_base_image=False,
+        resize_touple=(0, 0),
+    ):
         images_files_array = self.images_files_array
         path = self.path
         video_info = self.video_info
@@ -49,7 +57,9 @@ class Mask:
                 output_image = None
 
                 if mask_type == "centroid":
-                    output_image = self.centroid(p1, p2, cap, show_centroid)
+                    output_image = self.centroid(
+                        p1, p2, cap, show_centroid, include_base_image
+                    )
                 elif mask_type == "convex":
                     output_image = self.convex(p1, p2, cap)
                 elif mask_type == "simple_sort":
@@ -57,19 +67,33 @@ class Mask:
                 elif mask_type == "points":
                     output_image = self.points(p1, p2, cap)
                 else:
-                    print("There is no mask type called", mask_type)
+                    print("There is no mask type called:", mask_type)
                     return
 
                 if include_points:
                     output_image = self.add_points_to_mask(p1, p2, output_image)
 
-                img_rsz = cv2.resize(output_image, (500, 500))
-                display(Image(data=cv2.imencode(".jpg", img_rsz)[1].tobytes()))
+                if not (resize_touple[0] == 0 or resize_touple[1] == 0):
+                    output_image = cv2.resize(output_image, (500, 500))
 
-    def save(self, mask_type="centroid", show_centroid=False, include_points=False):
-        print("Images saving not implemented yet")
+                if action == "save":
+                    name = os.path.join(
+                        VAL_MASK_DIR, file[0:-4] + "_" + str(frame) + ".jpg"
+                    )
+                    cv2.imwrite(name, output_image)
 
-    def points(self, p1, p2, cap):
+                    _, frame_image = cap.read()
+                    name_image = os.path.join(
+                        VAL_IMG_DIR, file[0:-4] + "_" + str(frame) + ".jpg"
+                    )
+                    cv2.imwrite(name_image, frame_image)
+
+                else:
+                    display(Image(data=cv2.imencode(".jpg", output_image)[1].tobytes()))
+        if action == "save":
+            print("IMAGE SAVING DONE!")
+
+    def points(self, p1, p2, cap, include_base_image):
         ret, img = cap.read()
         if ret:
             for point in p1:
@@ -80,7 +104,7 @@ class Mask:
 
         return img
 
-    def centroid(self, p1, p2, cap, show_centroid):
+    def centroid(self, p1, p2, cap, show_centroid, include_base_image=False):
         points = np.concatenate((p1, p2), axis=0)
 
         centroid = np.mean(points, axis=0)
@@ -93,18 +117,22 @@ class Mask:
 
         ret, img = cap.read()
         if ret:
-            polygon = cv2.fillPoly(img, [poly], (255, 255, 255, 90))
-            if show_centroid:
-                cv2.circle(
-                    polygon,
-                    (int(centroid[0]), int(centroid[1])),
-                    1,
-                    (0, 255, 0),
-                    -1,
-                )
+            if include_base_image:
+                polygon = cv2.fillPoly(img, [poly], (255, 255, 255, 90))
+                if show_centroid:
+                    cv2.circle(
+                        polygon,
+                        (int(centroid[0]), int(centroid[1])),
+                        1,
+                        (0, 255, 0),
+                        -1,
+                    )
+            else:
+                img = np.zeros(img.shape[:2], dtype=np.uint8)
+                cv2.fillPoly(img, [poly], 255)
         return img
 
-    def convex(self, p1, p2, cap):
+    def convex(self, p1, p2, cap, include_base_image):
         points = np.concatenate((p1, p2), axis=0).tolist()
         sorted_points = cv2.convexHull(np.array(points, dtype=np.int32))
         sorted_points = np.vstack((sorted_points, [sorted_points[0]]))
@@ -118,7 +146,7 @@ class Mask:
 
         return img
 
-    def simple_sort(self, p1, p2, cap):
+    def simple_sort(self, p1, p2, cap, include_base_image):
         sorted_puntos1 = sorted(p1, key=lambda x: x[1])
         sorted_puntos2 = sorted(p2, key=lambda x: x[1])
 
