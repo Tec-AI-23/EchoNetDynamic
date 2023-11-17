082d59d7af885f9a98cb1dc6037a385be78ffeab
CristianCazares <cm_cjavier@hotmail.com>
2023-11-17 15:03:21 -0600

    feat(file_pats): added a single script to declare all the data paths and implemented on dataExtraction notebook

---
+++ scripts/FILE_PATHS.py
@@ -0,0 +1,10 @@
+# For now, this are only meant to work on the /scripts folder.
+
+ECHONET = "../EchoNet-Dynamic"
+VIDEOS = "../EchoNet-Dynamic/videos"
+
+DATA = "../EchoNet-Dynamic/data"
+IMAGES = "../EchoNet-Dynamic/data/images"
+MAKS = "../EchoNet-Dynamic/data/masks"
+
+HEATMAPS = "../EchoNet-Dynamic/data/heatmaps"
